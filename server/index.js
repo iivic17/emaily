d3c572be83a2afa1228ccd38d392b67068d912ba
iivic17 <ilijaivic17@gmail.com>
2022-10-16 21:40:19 +0200

    Passport initialized

--- server/index.js
+++ server/index.js
@@ -1,12 +1,10 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+
 const app = express();
 
-app.get('/', (req, res) => {
-	res.send({
-		status: 'ok',
-	});
-});
+passport.use(new GoogleStrategy());
 
 const PORT = process.env.PORT ?? 3000;
-
 app.listen(PORT);
