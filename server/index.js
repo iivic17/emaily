eff0d9d008e2d53993d748d6e9f1fc276fe23537
iivic17 <ilijaivic17@gmail.com>
2022-10-18 15:58:29 +0200

    Enabling cookies

--- server/index.js
+++ server/index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
 const keys = require('./config/keys');
 require('./models/User');
 require('./services/passport');
@@ -7,6 +9,16 @@ require('./services/passport');
 mongoose.connect(keys.mongo.URI);
 
 const app = express();
+
+app.use(
+	cookieSession({
+		maxAge: 30 * 24 * 60 * 60 * 1000,
+		keys: [keys.cookie.key],
+	})
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 require('./routes/auth.routes')(app);
 
