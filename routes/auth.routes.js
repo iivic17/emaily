5808c48a281db776894dac73096ee7df5a36bd89
iivic17 <ilijaivic17@gmail.com>
2022-10-22 23:11:24 +0200

    Redirecting a user on auth

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -17,18 +17,16 @@ module.exports = app => {
 	app.get('/auth/facebook', passport.authenticate('facebook'));
 
 	app.get('/auth/google/callback', passport.authenticate('google'), (req, res) => {
-		res.redirect('/api/current_user');
+		res.redirect('/surveys');
 	});
 
 	app.get('/auth/facebook/callback', passport.authenticate('facebook'), (req, res) => {
-		res.redirect('/api/current_user');
+		res.redirect('/surveys');
 	});
 
 	app.get('/api/logout', (req, res) => {
 		req.logout();
-		res.send({
-			status: 'ok',
-		});
+		res.redirect('/');
 	});
 
 	app.get('/api/current_user', (req, res) => {
