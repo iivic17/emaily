9d810421b96bbd8a52be81d005b34ae6f39a5cbf
iivic17 <ilijaivic17@gmail.com>
2022-10-24 21:11:34 +0200

    Handling surveys post request

--- routes/survey.routes.js
+++ routes/survey.routes.js
@@ -1,6 +1,20 @@
+const mongoose = require('mongoose');
 const requireCredits = require('./../middlewares/requireCredits');
 const requireLogin = require('./../middlewares/requireLogin');
 
+const Survey = mongoose.model('surveys');
+
 module.exports = app => {
-	app.post('/surveys', requireLogin, requireCredits, (req, res) => {});
+	app.post('/surveys', requireLogin, requireCredits, (req, res) => {
+		const { title, subject, body, recipients } = req.body;
+
+		const survey = new Survey({
+			title,
+			subject,
+			body,
+			recipients: recipients.split(',').map(email => ({ email: email.trim() })),
+			_user: req.user.id,
+			dateSent: Date.now(),
+		});
+	});
 };
