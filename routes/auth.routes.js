e345d3ee35b1ea32ab9559f153002a1198b9cfba
iivic17 <ilijaivic17@gmail.com>
2022-10-24 13:24:41 +0200

    Removed root route handler

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,12 +1,6 @@
 const passport = require('passport');
 
 module.exports = app => {
-	app.get('/', (req, res) => {
-		res.send({
-			status: 'ok',
-		});
-	});
-
 	app.get(
 		'/auth/google',
 		passport.authenticate('google', {
