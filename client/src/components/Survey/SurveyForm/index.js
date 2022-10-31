12b76b2b9fa82665aac605100c6165bbf2d06625
iivic17 <ilijaivic17@gmail.com>
2022-10-31 18:31:15 +0100

    New survey form initial UI with hook forms

--- client/src/components/Survey/SurveyForm/index.js
+++ client/src/components/Survey/SurveyForm/index.js
@@ -1,7 +1,62 @@
 import React from 'react';
+import { useForm } from 'react-hook-form';
+import { ErrorMessage } from '@hookform/error-message';
 
 const SurveyForm = () => {
-	return <div>Survey Form</div>;
+	const {
+		register,
+		handleSubmit,
+		formState: { errors },
+	} = useForm({
+		mode: 'onTouched',
+		defaultValues: {
+			surveySubject: '',
+			surveyTitle: '',
+			emailBody: '',
+			recipientList: '',
+		},
+		delayError: 300,
+	});
+	const onSubmit = data => {
+		console.log(data);
+	};
+
+	console.log(errors);
+
+	return (
+		<form onSubmit={handleSubmit(onSubmit)}>
+			{/* Survey Subject */}
+			<input
+				{...register('surveySubject', {
+					required: 'Survey subject is required.',
+				})}
+				type='text'
+			/>
+			<ErrorMessage errors={errors} name='surveySubject' />
+
+			{/* Survey Title */}
+			<input {...register('surveyTitle', { required: true })} type='text' />
+			<ErrorMessage errors={errors} name='surveyTitle' />
+
+			{/* Email Body */}
+			<input {...register('emailBody', { required: true })} type='text' />
+			<ErrorMessage errors={errors} name='emailBody' />
+
+			{/* Recipient List */}
+			<input
+				{...register('recipientList', {
+					required: true,
+					validate: false,
+				})}
+				type='text'
+			/>
+			<ErrorMessage errors={errors} name='recipientList' />
+
+			<button className='button' type='submit'>
+				Next
+			</button>
+		</form>
+	);
 };
 
 export default SurveyForm;
