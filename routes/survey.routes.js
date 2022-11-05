30808d86c84c95d3bf5e83c939a63e1d2353306e
Ilija Ivic <ilijaivic17@gmail.com>
2022-11-05 15:23:46 +0100

    Surveys get route handled

--- routes/survey.routes.js
+++ routes/survey.routes.js
@@ -8,6 +8,14 @@ const { URL } = require('url');
 const Survey = mongoose.model('surveys');
 
 module.exports = app => {
+	app.get('/api/surveys', requireLogin, async (req, res) => {
+		const surveys = await Survey.find({ _user: req.user.id }).select({
+			recipients: false,
+		});
+
+		res.send(surveys);
+	});
+
 	app.post('/api/surveys', requireLogin, requireCredits, async (req, res) => {
 		const { title, subject, body, recipients } = req.body;
 
