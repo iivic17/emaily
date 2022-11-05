e9d80172126d1e17b6321ff67a20bc21e5367961
Ilija Ivic <ilijaivic17@gmail.com>
2022-11-06 00:35:44 +0100

    Survey raw cards displayed

--- client/src/components/Dashboard/index.js
+++ client/src/components/Dashboard/index.js
@@ -2,22 +2,40 @@ import React, { useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import SvgDef from '../SvgDef';
 import './Dashboard.scss';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 import { PlusIcon } from '../../Icons/PlusIcon';
 import { fetchSurveys } from '../../store/surveys';
 
 const Dashboard = () => {
 	const dispatch = useDispatch();
+	const surveys = useSelector(state => state.surveys.data);
 
 	useEffect(() => {
 		dispatch(fetchSurveys());
 	}, [dispatch]);
 
+	const renderSurveys = () => {
+		return surveys.map(({ _id, title, body, subject, yes, no, dateSent }) => {
+			return (
+				<div className='survey' key={_id}>
+					<h3>{title}</h3>
+					<h4>{subject}</h4>
+					<p>{body}</p>
+					<span>yes:</span> <span>{yes}</span>
+					<span>no:</span> <span>{no}</span>
+					<time>{dateSent}</time>
+				</div>
+			);
+		});
+	};
+
 	return (
 		<div className='dashboard'>
-			<Link to='/surveys/new'>
-				<button className='button button-floating button-circle new-survey-button'>
+			{renderSurveys()}
+
+			<Link to='/surveys/new' className='fixed-bottom-right'>
+				<button className='button button-circle new-survey-button'>
 					<SvgDef>
 						<PlusIcon useGradient />
 					</SvgDef>
