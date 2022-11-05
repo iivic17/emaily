201c42bad1c65d07de536fe40465c6b4195499db
Ilija Ivic <ilijaivic17@gmail.com>
2022-11-05 20:40:33 +0100

    Landing page restyled

--- client/src/components/Landing/index.js
+++ client/src/components/Landing/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import './Landing.scss';
+import { Player } from '@lottiefiles/react-lottie-player';
+import * as animationData from '../../animations/landing-illustration.json';
 
 const Landing = () => {
 	return (
@@ -11,6 +13,55 @@ const Landing = () => {
 				<br />
 				Value Your <span className='heading-underline'>Partnership</span>,
 			</h2>
+			<div className='landing-main-cols'>
+				<div className='landing-text-box'>
+					<p>
+						When it comes to new products,
+						<br /> services or software, the Emaily
+						<br />
+						team believes in the notion of
+						<br />
+					</p>
+					<span className='gradient-text bold'>
+						“design - build - validate”.
+					</span>
+					<p>
+						With our team of experts, we give your
+						<br />
+						go-to-market strategy wings and cater
+						<br />
+						to your needs with knowledge, passion
+						<br />
+						and speed.
+					</p>
+					<p>
+						We are here to help you in a multitude
+						<br />
+						of ways that aren't designed to just
+						<br />
+						deliver a new product or service, as we
+						<br />
+						are equally keen to forge a long-term
+						<br />
+						relationship with your team and the
+						<br />
+						wider company.
+					</p>
+					<p>
+						When working with Emaily, you get
+						<br />
+						to select a working relationship from a<br />
+						variety of options, and we couldn’t be
+						<br />
+						more thrilled to get the ball rolling on
+						<br />
+						your project.
+					</p>
+				</div>
+				<div className='landing-animation-box'>
+					<Player src={animationData} className='player' loop autoplay />
+				</div>
+			</div>
 		</div>
 	);
 };
